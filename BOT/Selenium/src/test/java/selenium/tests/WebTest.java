ca90229d8d3debc1a9b1ac45840509e7674df1b4
niravdsouza <nedsouza@ncsu.edu>
2017-10-25 02:24:09 -0400

    Selenium Test code Added

--- BOT/Selenium/src/test/java/selenium/tests/WebTest.java
+++ BOT/Selenium/src/test/java/selenium/tests/WebTest.java
@@ -53,7 +53,7 @@ public class WebTest
 		driver.quit();
 	}
 
-
+/*
 	
 	@Test
 	public void StartStandup() throws Exception
@@ -62,8 +62,54 @@ public class WebTest
 		WebDriverWait wait = new WebDriverWait(driver, 70);
     	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='btn btn_attachment'and contains(., 'Start')]")));
     	List<WebElement> Start = driver.findElements(By.xpath("//button[@class='btn btn_attachment'and contains(., 'Start')]"));
-    	Start.get(0).click();
-    	Thread.sleep(5000); 
+    	Thread.sleep(3000);
+    	Start.get(Start.size() - 1).click();
+    	Thread.sleep(3000); 
+    	
+    	// Configuring start time
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'What did you accomplish yesterday?']")));
+        WebElement startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'What did you accomplish yesterday?']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
+        
+        WebElement bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("I completed the DevOps Test Analysis Milestone");
+        bot.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+        
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'What will you work on today?']")));
+        startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'What will you work on today?']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
+        
+        bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("Will be working on DevOps Deployment Milestone");
+        bot.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+        
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'Is there anything blocking your progress?']")));
+        startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'Is there anything blocking your progress?']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
+        
+        bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("Not yet");
+        bot.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+        
+        bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("Not yet");
+        bot.sendKeys(Keys.RETURN);
+        
+        driver.get("https://parkwoodgang.slack.com/messages/C7HTHUL3B/");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'whatbot.ncsu has completed the standup. The reponses are as follows-']")));
+        startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'whatbot.ncsu has completed the standup. The reponses are as follows-']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
 	}
 	
 	@Test
@@ -75,7 +121,57 @@ public class WebTest
     	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='btn btn_attachment'and contains(., 'Start')]")));
     	List<WebElement> Snooze = driver.findElements(By.xpath("//button[@class='btn btn_attachment'and contains(., 'Snooze')]"));
     	Snooze.get(0).click();
-    	Thread.sleep(5000);    	
+    	Thread.sleep(5000);
+    	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='btn btn_attachment'and contains(., 'Start')]")));
+    	List<WebElement> Start = driver.findElements(By.xpath("//button[@class='btn btn_attachment'and contains(., 'Start')]"));
+    	Thread.sleep(3000);
+    	Start.get(Start.size() - 1).click();
+    	Thread.sleep(3000); 
+    	
+    	// Configuring start time
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'What did you accomplish yesterday?']")));
+        WebElement startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'What did you accomplish yesterday?']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
+        
+        WebElement bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("I completed the DevOps Test Analysis Milestone");
+        bot.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+        
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'What will you work on today?']")));
+        startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'What will you work on today?']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
+        
+        bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("Will be working on DevOps Deployment Milestone");
+        bot.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+        
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'Is there anything blocking your progress?']")));
+        startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'Is there anything blocking your progress?']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
+        
+        bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("Not yet");
+        bot.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+        
+        bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("Not yet");
+        bot.sendKeys(Keys.RETURN);
+        
+        driver.get("https://parkwoodgang.slack.com/messages/C7HTHUL3B/");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'whatbot.ncsu has completed the standup. The reponses are as follows-']")));
+        startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'whatbot.ncsu has completed the standup. The reponses are as follows-']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
 	}
 	
 	@Test
@@ -88,5 +184,100 @@ public class WebTest
     	Ignore.get(0).click();
     	Thread.sleep(5000); 
 	}
+	*/	
+	@Test
+	public void RedoStandup() throws Exception
+	{
+		driver.get("https://parkwoodgang.slack.com/messages/D7LJ7H9U4/");
+		WebDriverWait wait = new WebDriverWait(driver, 70);
+    	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='btn btn_attachment'and contains(., 'Start')]")));
+    	List<WebElement> Start = driver.findElements(By.xpath("//button[@class='btn btn_attachment'and contains(., 'Start')]"));
+    	Thread.sleep(3000);
+    	Start.get(Start.size() - 1).click();
+    	Thread.sleep(3000); 
+    	
+    	// Configuring start time
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'What did you accomplish yesterday?']")));
+        WebElement startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'What did you accomplish yesterday?']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
+        
+        WebElement bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("I completed the DevOps Test Analysis Milestone");
+        bot.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+        
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'What will you work on today?']")));
+        startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'What will you work on today?']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
+        
+        bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("Will be working on DevOps Deployment Milestone");
+        bot.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+        
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'Is there anything blocking your progress?']")));
+        startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'Is there anything blocking your progress?']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
+        
+        bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("Not Yet");
+        bot.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+        
+        bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("y");
+        bot.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+    	
+    	// Configuring start time
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'What did you accomplish yesterday?']")));
+        startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'What did you accomplish yesterday?']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
+        
+        bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("I completed the DevOps Test Analysis Milestone");
+        bot.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+        
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'What will you work on today?']")));
+        startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'What will you work on today?']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
+        
+        bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("Will be working on DevOps Deployment Milestone");
+        bot.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+        
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'Is there anything blocking your progress?']")));
+        startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'Is there anything blocking your progress?']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
+        
+        bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("Not yet");
+        bot.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+        
+        bot = driver.findElement(By.xpath("//div[@id='msg_input']/div")); // ("msg_input").);
+        bot.sendKeys("Not yet");
+        bot.sendKeys(Keys.RETURN);
+        
+        driver.get("https://parkwoodgang.slack.com/messages/C7HTHUL3B/");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'whatbot.ncsu has completed the standup. The reponses are as follows-']")));
+        startTime = driver.findElement(
+                By.xpath("//span[contains(@class,'message_body') and text() = 'whatbot.ncsu has completed the standup. The reponses are as follows-']"));
+        assertNotNull(startTime);
+        Thread.sleep(3000);
+	}
 }
-
